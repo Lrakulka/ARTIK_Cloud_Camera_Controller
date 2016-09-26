1bed8cbdc7a10533cc19e545badf2694b132ec5a
Lrakulka <Olecsandr32@gmail.com>
2016-09-26 20:24:32 +0200

    Controller ready

---
+++ app/src/main/java/lisp/artik_cloud_camera_controller/ArtikCloudSession.java
@@ -0,0 +1,395 @@
+/*
+ * Copyright (C) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Modifications copyright (C) 2016 Oleksandr Borysov
+ */
+
+package lisp.artik_cloud_camera_controller;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import cloud.artik.api.UsersApi;
+import cloud.artik.model.Acknowledgement;
+import cloud.artik.model.ActionDetails;
+import cloud.artik.model.ActionDetailsArray;
+import cloud.artik.model.ActionIn;
+import cloud.artik.model.ActionOut;
+import cloud.artik.model.MessageOut;
+import cloud.artik.model.RegisterMessage;
+import cloud.artik.model.WebSocketError;
+import cloud.artik.websocket.ArtikCloudWebSocketCallback;
+import cloud.artik.websocket.DeviceChannelWebSocket;
+import cloud.artik.websocket.FirehoseWebSocket;
+
+public class ArtikCloudSession {
+    private final static String TAG = ArtikCloudSession.class.getSimpleName();
+
+    // Copy from the corresponding application in the Developer Dashboard
+    public static final String CLIENT_ID = "5bf4498c939b40f1a0ded23b8e093e83";
+
+    // Copy from the Device Info screen in My ARTIK Cloud
+    private final static String DEVICE_ID = "0b5cfbecdfa04fbfa8b7a331c85fb6a0";
+
+    private static final String ARTIK_CLOUD_AUTH_BASE_URL = "https://accounts.artik.cloud";
+    // Response of application when success authorised
+    public static final String REDIRECT_URL = "android-app://redirect";
+
+    private final static String DEVICE_NAME = "Android Camera Tracker";
+    // Actions must be defined in manifest
+    private final static String ACTION_MAKE_PHOTO = "makePhoto";
+    private final static String ACTION_START_VIDEO = "setOnVideo";
+    private final static String ACTION_STOP_VIDEO = "setOffVideo";
+
+    private static ArtikCloudSession ourInstance = new ArtikCloudSession();
+    private static Context ourContext;
+
+    public final static String WEBSOCKET_LIVE_ONOPEN =
+            "cloud.artik.example.iot.WEBSOCKET_LIVE_ONOPEN";
+    public final static String WEBSOCKET_LIVE_ONMSG =
+            "cloud.artik.example.iot.WEBSOCKET_LIVE_ONMSG";
+    public final static String WEBSOCKET_LIVE_ONCLOSE =
+            "cloud.artik.example.iot.WEBSOCKET_LIVE_ONCLOSE";
+    public final static String WEBSOCKET_LIVE_ONERROR =
+            "cloud.artik.example.iot.WEBSOCKET_LIVE_ONERROR";
+    public final static String WEBSOCKET_WS_ONOPEN =
+            "cloud.artik.example.iot.WEBSOCKET_WS_ONOPEN";
+    public final static String WEBSOCKET_WS_ONREG =
+            "cloud.artik.example.iot.WEBSOCKET_WS_ONREG";
+    public final static String WEBSOCKET_WS_ONMSG =
+            "cloud.artik.example.iot.WEBSOCKET_WS_ONMSG";
+    public final static String WEBSOCKET_WS_ONACK =
+            "cloud.artik.example.iot.WEBSOCKET_WS_ONACK";
+    public final static String WEBSOCKET_WS_ONCLOSE =
+            "cloud.artik.example.iot.WEBSOCKET_WS_ONCLOSE";
+    public final static String WEBSOCKET_WS_ONERROR =
+            "cloud.artik.example.iot.WEBSOCKET_WS_ONERROR";
+    public final static String SDID = "sdid";
+    public final static String DEVICE_DATA = "data";
+    public final static String TIMESTEP = "ts";
+    public final static String ACK = "ack";
+    public final static String ERROR = "error";
+
+    private UsersApi mUsersApi = null;
+    private String mAccessToken = null;
+    private String mUserId = null;
+
+    private FirehoseWebSocket mFirehoseWS = null; //  end point: /live
+    private DeviceChannelWebSocket mDeviceChannelWS = null; // end point: /websocket
+
+    public static ArtikCloudSession getInstance() {
+        return ourInstance;
+    }
+
+    private ArtikCloudSession() {
+        // Do nothing
+    }
+
+    public void setContext(Context context) {
+        ourContext = context;
+    }
+
+    public String getDeviceID() {
+        return DEVICE_ID;
+    }
+
+    public String getDeviceName() {
+        return DEVICE_NAME;
+    }
+
+    public void setAccessToken(String token) {
+        if (token == null || token.length() <= 0) {
+            Log.e(TAG, "Attempt to set an invalid token");
+            mAccessToken = null;
+            return;
+        }
+        mAccessToken = token;
+    }
+
+    public String getAuthorizationRequestUri() {
+         //https://accounts.artik.cloud/authorize?client=mobile&client_id=xxxx&response_type=token&redirect_uri=http://localhost:8000/acdemo/index.php
+        return ARTIK_CLOUD_AUTH_BASE_URL + "/authorize?client=mobile&response_type=token&" +
+                "client_id=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URL;
+    }
+
+    public void reset() {
+        mUsersApi = null;
+        mAccessToken = null;
+        mUserId = null;
+        mFirehoseWS = null;
+        mDeviceChannelWS = null;
+    }
+
+    private void createFirehoseWebsocket() {
+        try {
+            mFirehoseWS = new FirehoseWebSocket(mAccessToken, DEVICE_ID, null, null, null, new ArtikCloudWebSocketCallback() {
+                @Override
+                public void onOpen(int i, String s) {
+                    Log.d(TAG, "FirehoseWebSocket: onOpen()");
+                    final Intent intent = new Intent(WEBSOCKET_LIVE_ONOPEN);
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+                }
+
+                @Override
+                public void onMessage(MessageOut messageOut) {
+                    Log.d(TAG, "FirehoseWebSocket: onMessage(" + messageOut.toString() + ")");
+                    final Intent intent = new Intent(WEBSOCKET_LIVE_ONMSG);
+                    intent.putExtra(SDID, messageOut.getSdid());
+                    intent.putExtra(DEVICE_DATA, messageOut.getData().toString());
+                    intent.putExtra(TIMESTEP, messageOut.getTs().toString());
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+                }
+
+                @Override
+                public void onAction(ActionOut actionOut) {
+                }
+
+                @Override
+                public void onAck(Acknowledgement acknowledgement) {
+                }
+
+                @Override
+                public void onClose(int code, String reason, boolean remote) {
+                    final Intent intent = new Intent(WEBSOCKET_LIVE_ONCLOSE);
+                    intent.putExtra("error", "mFirehoseWS is closed. code: " + code + "; reason: " + reason);
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+                }
+
+                @Override
+                public void onError(WebSocketError ex) {
+                    final Intent intent = new Intent(WEBSOCKET_LIVE_ONERROR);
+                    intent.putExtra("error", "mFirehoseWS error: " + ex.getMessage());
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+                }
+
+                @Override
+                public void onPing(long timestamp) {
+                    Log.d(TAG, "FirehoseWebSocket::onPing: " + timestamp);
+                }
+            });
+        } catch (URISyntaxException|IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Closes a websocket /live connection
+     */
+    public void disconnectFirehoseWS() {
+        if (mFirehoseWS != null) {
+            try {
+                mFirehoseWS.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        mFirehoseWS = null;
+    }
+
+    public void connectFirehoseWS() {
+        createFirehoseWebsocket();
+        try {
+            mFirehoseWS.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createDeviceChannelWebSockets() {
+        try {
+            mDeviceChannelWS = new DeviceChannelWebSocket(true, new ArtikCloudWebSocketCallback() {
+                @Override
+                public void onOpen(int i, String s) {
+                    Log.d(TAG, "Registering " + DEVICE_ID);
+                    final Intent intent = new Intent(WEBSOCKET_WS_ONOPEN);
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+
+                    RegisterMessage registerMessage = new RegisterMessage();
+                    registerMessage.setAuthorization("bearer " + mAccessToken);
+                    registerMessage.setCid("myRegisterMessage");
+                    registerMessage.setSdid(DEVICE_ID);
+
+                    try {
+                        Log.d(TAG, "DeviceChannelWebSocket::onOpen: registering" + DEVICE_ID);
+                        mDeviceChannelWS.registerChannel(registerMessage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onMessage(MessageOut messageOut) {
+                    Log.d(TAG, "DeviceChannelWebSocket::onMessage(" + messageOut.toString());
+                    final Intent intent = new Intent(WEBSOCKET_WS_ONMSG);
+                    intent.putExtra(ACK, messageOut.toString());
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+                }
+
+                @Override
+                public void onAction(ActionOut actionOut) {
+
+                }
+
+                @Override
+                public void onAck(Acknowledgement acknowledgement) {
+                    Log.d(TAG, "DeviceChannelWebSocket::onAck(" + acknowledgement.toString());
+                    Intent intent;
+                    if (acknowledgement.getMessage() != null && acknowledgement.getMessage().equals("OK")) {
+                        intent = new Intent(WEBSOCKET_WS_ONREG);
+                    } else {
+                        intent = new Intent(WEBSOCKET_WS_ONACK);
+                        intent.putExtra(ACK, acknowledgement.toString());
+                    }
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+                }
+
+                @Override
+                public void onClose(int code, String reason, boolean remote) {
+                    final Intent intent = new Intent(WEBSOCKET_WS_ONCLOSE);
+                    intent.putExtra(ERROR, "mWebSocket is closed. code: " + code + "; reason: " + reason);
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+
+                }
+
+                @Override
+                public void onError(WebSocketError error) {
+                    final Intent intent = new Intent(WEBSOCKET_WS_ONERROR);
+                    intent.putExtra(ERROR, "mWebSocket error: " + error.getMessage());
+                    LocalBroadcastManager.getInstance(ourContext).sendBroadcast(intent);
+                }
+
+                @Override
+                public void onPing(long timestamp) {
+                    Log.d(TAG, "DeviceChannelWebSocket::onPing: " + timestamp);
+                }
+            });
+        } catch (URISyntaxException|IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Closes a websocket /websocket connection
+     */
+    public void disconnectDeviceChannelWS() {
+        if (mDeviceChannelWS != null) {
+            try {
+                mDeviceChannelWS.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        mDeviceChannelWS = null;
+    }
+
+    public void connectDeviceChannelWS() {
+        createDeviceChannelWebSockets();
+        try {
+            mDeviceChannelWS.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Send action "Make Photo" to ARTIK Cloud
+     */
+    public void sendMakePhotoActionInDeviceChannelWS() {
+        new sendActionInBackground().execute(ACTION_MAKE_PHOTO);
+    }
+
+    /**
+     * Send action "Start Recording" to ARTIK Cloud
+     */
+    public void sendStartVideoActionInDeviceChannelWS() {
+        new sendActionInBackground().execute(ACTION_START_VIDEO);
+    }
+
+    /**
+     * Send action "Stop Recording" to ARTIK Cloud
+     */
+    public void sendStopVideoActionInDeviceChannelWS() {
+        new sendActionInBackground().execute(ACTION_STOP_VIDEO);
+    }
+
+    /*
+     * Example of Action sent to ARTIK Cloud over /websocket endpoint
+     *  {
+        cid:  setOff
+        data:  {
+                 actions: [
+                            {
+                              name:  setOff
+                              parameter: {}
+                            }
+                          ]
+               }
+        ddid:  fde8715961f84798a841be23480b8ce5
+        sdid:  null
+        ts:   1451606965889
+        }
+     *
+     */
+    private void sendActionInDeviceChannelWS(String actionName) {
+        ActionIn actionIn = new ActionIn();
+        ActionDetails action = new ActionDetails();
+        ArrayList<ActionDetails> actions = new ArrayList<>();
+        ActionDetailsArray actionDetailsArray = new ActionDetailsArray();
+
+        action.setName(actionName);
+        actions.add(action);
+        actionDetailsArray.setActions(actions);
+        actionIn.setData(actionDetailsArray);
+        actionIn.setCid(actionName);
+        actionIn.setDdid(DEVICE_ID);
+        actionIn.setTs(System.currentTimeMillis());
+
+        try {
+            mDeviceChannelWS.sendAction(actionIn);
+            Log.d(TAG, "DeviceChannelWebSocket sendAction:" + actionIn.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    class sendActionInBackground extends AsyncTask<String, Void, Void> {
+        final static String TAG = "sendActionInBackground";
+        @Override
+        protected Void doInBackground(String... actionName) {
+            try {
+                sendActionInDeviceChannelWS(actionName[0]);
+            } catch (Exception e) {
+                Log.v(TAG, "::doInBackground run into Exception");
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // Do nothing!
+        }
+    }
+
+
+}
--- app/src/main/java/lisp/artik_cloud_camera_controller/MainActivity.java
+++ app/src/main/java/lisp/artik_cloud_camera_controller/MainActivity.java
@@ -1,13 +1,201 @@
+/*
+ * Copyright (C) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Modifications copyright (C) 2016 Oleksandr Borysov
+ */
+
 package lisp.artik_cloud_camera_controller;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView mLiveStatus;
+    private TextView mDeviceStatus;
+    private TextView mStatusUpdateTime;
+    private TextView mWSStatus;
+    private TextView mWSReceived; // received ack or msg at /websocket end point
+
+    private static final String WS_HEADER = "WebSocket /websocket: ";
+    private static final String LIVE_HEADER = "WebSocket /live: ";
+    private static final String DEVICE_REGISTERED = "device registered ";
+    private static final String CONNECTED = "connected ";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TextView deviceName;
+        TextView deviceID;
+        Button onBtn;
+        Button offBtn;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLiveStatus = (TextView)findViewById(R.id.live_status);
+        deviceName = (TextView)findViewById(R.id.device_name);
+        deviceID = (TextView)findViewById(R.id.device_id);
+        mDeviceStatus = (TextView)findViewById(R.id.device_status);
+        mStatusUpdateTime = (TextView)findViewById(R.id.status_update_time);
+
+        mWSStatus = (TextView)findViewById(R.id.ws_status);
+        mWSReceived = (TextView)findViewById(R.id.ws_received);
+
+        setTitle(R.string.device_monitor_title);
+
+        ArtikCloudSession.getInstance().setContext(this);
+        deviceID.setText("Device ID: " + ArtikCloudSession.getInstance().getDeviceID());
+        deviceName.setText("Device Name: " + ArtikCloudSession.getInstance().getDeviceName());
+
+        onBtn = (Button)findViewById(R.id.make_photo);
+        onBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Log.v(TAG, ": make photo button is clicked.");
+                    ArtikCloudSession.getInstance().sendMakePhotoActionInDeviceChannelWS();
+                } catch (Exception e) {
+                    Log.v(TAG, "Run into Exception");
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        offBtn = (Button)findViewById(R.id.start_recording);
+        offBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Log.v(TAG, ": start video button is clicked.");
+                    ArtikCloudSession.getInstance().sendStartVideoActionInDeviceChannelWS();
+                } catch (Exception e) {
+                    Log.v(TAG, "Run into Exception");
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        offBtn = (Button)findViewById(R.id.button_stop_recording);
+        offBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Log.v(TAG, ": stop video button is clicked.");
+                    ArtikCloudSession.getInstance().sendStopVideoActionInDeviceChannelWS();
+                } catch (Exception e) {
+                    Log.v(TAG, "Run into Exception");
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mWSUpdateReceiver,
+                makeWebsocketUpdateIntentFilter());
+        mLiveStatus.setText("Connecting to /live ... ");
+        ArtikCloudSession.getInstance().connectFirehoseWS();//non blocking
+        mWSStatus.setText("Connecting to /websocket ...");
+        ArtikCloudSession.getInstance().connectDeviceChannelWS();//non blocking
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ArtikCloudSession.getInstance().disconnectFirehoseWS();
+        ArtikCloudSession.getInstance().disconnectDeviceChannelWS();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mWSUpdateReceiver);
+    }
+
+
+    private static IntentFilter makeWebsocketUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_LIVE_ONOPEN);
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_LIVE_ONMSG);
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_LIVE_ONCLOSE);
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_LIVE_ONERROR);
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_WS_ONOPEN);
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_WS_ONREG);
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_WS_ONMSG);
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_WS_ONACK);
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_WS_ONCLOSE);
+        intentFilter.addAction(ArtikCloudSession.WEBSOCKET_WS_ONERROR);
+        return intentFilter;
+    }
+
+    private final BroadcastReceiver mWSUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (ArtikCloudSession.WEBSOCKET_LIVE_ONOPEN.equals(action)) {
+                displayLiveStatus(LIVE_HEADER + CONNECTED);
+            } else if (ArtikCloudSession.WEBSOCKET_LIVE_ONMSG.equals(action)) {
+                String status = intent.getStringExtra(ArtikCloudSession.DEVICE_DATA);
+                String updateTime = intent.getStringExtra(ArtikCloudSession.TIMESTEP);
+                displayDeviceStatus(status, updateTime);
+            } else if (ArtikCloudSession.WEBSOCKET_LIVE_ONCLOSE.equals(action) ||
+                    ArtikCloudSession.WEBSOCKET_LIVE_ONERROR.equals(action)) {
+                displayLiveStatus(LIVE_HEADER + intent.getStringExtra(ArtikCloudSession.ERROR));
+            } else  if (ArtikCloudSession.WEBSOCKET_WS_ONOPEN.equals(action)) {
+                displayWSStatus(WS_HEADER + CONNECTED);
+            } else  if (ArtikCloudSession.WEBSOCKET_WS_ONREG.equals(action)) {
+                displayWSStatus(WS_HEADER + DEVICE_REGISTERED);
+            } else if (ArtikCloudSession.WEBSOCKET_WS_ONMSG.equals(action)) {
+                displayWSReceived(intent.getStringExtra("msg"));
+            } else if (ArtikCloudSession.WEBSOCKET_WS_ONACK.equals(action)) {
+                displayWSReceived(intent.getStringExtra(ArtikCloudSession.ACK));
+            } else if (ArtikCloudSession.WEBSOCKET_WS_ONCLOSE.equals(action) ||
+                    ArtikCloudSession.WEBSOCKET_WS_ONERROR.equals(action)) {
+                displayWSStatus(WS_HEADER + intent.getStringExtra(ArtikCloudSession.ERROR));
+            }
+
+        }
+    };
+
+    private void displayLiveStatus(String status) {
+        Log.d(TAG, status);
+        mLiveStatus.setText(status);
+    }
+
+    private void displayDeviceStatus(String status, String updateTimems) {
+        mDeviceStatus.setText(status);
+        long time_ms = Long.parseLong(updateTimems);
+        mStatusUpdateTime.setText(DateFormat.getDateTimeInstance().format(new Date(time_ms)));
+    }
+
+    private void displayWSStatus(String status) {
+        Log.d(TAG, status);
+        mWSStatus.setText(status);
+    }
+
+    private void displayWSReceived(String status) {
+        Log.d(TAG, status);
+        mWSReceived.setText(status);
+    }
+
 }
