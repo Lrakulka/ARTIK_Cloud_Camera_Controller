036725b3f6040d0003fbadc1520606bce7ecda13
Lrakulka <Olecsandr32@gmail.com>
2016-09-26 16:38:23 +0200

    Initial commit

---
+++ app/src/main/java/lisp/artik_cloud_camera_controller/MainActivity.java
@@ -0,0 +1,13 @@
+package lisp.artik_cloud_camera_controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
